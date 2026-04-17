b79857344bc3ca6f9b4f102ebc93cabb808e5a22
TheNhung35 <nguyenvanthe03051995@gmail.com>
2026-04-17 13:24:53 +0700

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -7,8 +7,5 @@ os.environ['PYTHONIOENCODING'] = 'utf-8'
 # Add current directory to path
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Import WSGI app directly
+# Import WSGI app directly - simple and clean
 from app import app as application
-
-# Create callable for Passenger
-application
