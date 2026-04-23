8501d25c9f670466816b353f0f2e65527984574f
NguyenVanThe30081994 <54802610+NguyenVanThe30081994@users.noreply.github.com>
2026-04-23 23:43:17 +0700

    Update seed_reporting_data.py

--- seed_reporting_data.py
+++ seed_reporting_data.py
@@ -15,7 +15,7 @@ from app import app
 from models_reporting import db, ReportingPeriod, FormTemplate, FormVersion, FormField
 import openpyxl
 
-db.init_app(app)
+
 
 def seed_reporting_data():
     """Seed dữ liệu mẫu"""
