cf4eec581f364563044a5741c8568cd26094a3ea
NguyenVanThe30081994 <54802610+NguyenVanThe30081994@users.noreply.github.com>
2026-04-28 14:55:23 +0700

    1

--- models.py
+++ models.py
@@ -141,7 +141,7 @@ class Task(db.Model):
     author_name = db.Column(db.String(100))
     priority = db.Column(db.String(50))
     task_type = db.Column(db.String(100))
-    initial_status = db.Column(db.String(50), default='Chưa bắt đầu')
+    initial_status = db.Column(db.String(50), default='Chưa tiếp nhận')
     created_at = db.Column(db.DateTime, default=datetime.now)
     assignments = db.relationship('TaskAssignment', backref='task', cascade='all, delete-orphan')
 
@@ -150,7 +150,7 @@ class TaskAssignment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_id = db.Column(db.Integer, db.ForeignKey('task.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    status = db.Column(db.String(50), default='Chưa bắt đầu')
+    status = db.Column(db.String(50), default='Chưa tiếp nhận')
     result_file = db.Column(db.String(255))
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
     user = db.relationship('User', backref='task_assignments')
